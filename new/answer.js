53be941841515e236e3329d92d7b23ab90fd02ec
goldmedal <liugs963@gmail.com>
2014-05-30 11:49:08 +0800

    keydwon function

--- new/answer.js
+++ new/answer.js
@@ -2,14 +2,30 @@ $(document).ready( function() {
 
 	document.onkeydown = keyFunction;
 	
+}
+
+function nextQuestion(_qid) {
+
+	$.ajax({
+		url: 'answer_check.php',
+		type: 'post',
+		data: { qid : _qid },
+		success: { },
+		error: { },		
+
+
+	});
+}	
+
 
 }
 
+
 function keyFunction() {
+
 	if (event.keyCode==81 && event.altKey) {
 		$('#audioQuestion').play();	
 	}else if(event.keyCode==32){
 
-	}
 
 }
